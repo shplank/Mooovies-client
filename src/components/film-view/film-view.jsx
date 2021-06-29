6477faee03c974286db106e8a5826df2022dcaad
Joe Jordan <lankyjoe@gmail.com>
2021-06-29 16:33:52 -0700

    Added login view
    Added login view component and imported into main view

--- src/components/film-view/film-view.jsx
+++ src/components/film-view/film-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export class FilmView extends React.Component {
+
   render() {
     const { film, onBackClick } = this.props;
 
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
+
 import { FilmCard } from '../film-card/film-card';
 
 import { FilmView } from '../film-view/film-view';
@@ -10,7 +12,8 @@ export class MainView extends React.Component {
     super();
     this.state = {
       films: [],
-      selectedFilm: null
+      selectedFilm: null,
+      user: null
     }
   }
 
@@ -26,14 +29,22 @@ export class MainView extends React.Component {
       });
   }
 
-  setSelectedFilm(newSelectedFilm) {
+  setSelectedFilm(film) {
     this.setState({
-      selectedFilm: newSelectedFilm
+      selectedFilm: film
+    });
+  }
+
+  onLoggedIn(user) {
+    this.setState({
+      user
     });
   }
 
   render() {
     const { films, selectedFilm } = this.state;
+
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
     if (films.length === 0) return <div className="main-view" />;
 
