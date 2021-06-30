a043c7657056d4aa04aeaab7d5be5fbb91f7345b
Joe Jordan <lankyjoe@gmail.com>
2021-06-29 19:57:37 -0700

    Added prop types
    Added PropTypes

--- src/components/film-card/film-card.jsx
+++ src/components/film-card/film-card.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class FilmCard extends React.Component {
   render() {
     const { film, onFilmClick } = this.props;
-    return <div className="film-card" onClick={() => { onFilmClick(film); }}>{film.Title}</div>;
+    return (
+      <div onClick={() => onFilmClick(film)} className="film-card">{film.Title}</div>
+    );
   }
 }
+
+FilmCard.propTypes = {
+  film: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    ReleaseYear: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired
+  }).isRequired,
+  onFilmClick: PropTypes.func.isRequired
+};
