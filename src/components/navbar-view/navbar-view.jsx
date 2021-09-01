c72e37da31e955dbb55446f003f98d7a9f0cf9e2
Joe Jordan <lankyjoe@gmail.com>
2021-09-01 10:04:59 -0700

    Image path
    Updated image path for navbar

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Nav, Navbar, Container } from 'react-bootstrap';
 
-import logo from 'url:./MoooviesLogo.png';
+import logo from 'url:../../images/MoooviesLogo.png';
 
 import './navbar-view.scss';
 
@@ -17,24 +17,24 @@ render() {
 
     return (
     <Navbar className="navbar" bg="white" expand="md" fixed="top">
-          <Container>
-            <Navbar.Brand href={`/`}>
-              <img src={logo} alt="Mooovies logo" width="45" className="d-inline-block align-top" />
-              {' '}
-              Mooovies
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse className="justify-content-end">
-            <Nav>
-              <Nav.Link href={`/films`}>Films</Nav.Link>
-              <Nav.Link href={`/users/${user}`}>Profile</Nav.Link>
-              <Nav.Link href="#logout" onClick={logOut}>Logout</Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-    )
-    }
+      <Container>
+        <Navbar.Brand href={`/`}>
+          <img src={logo} alt="Mooovies logo" width="45" className="d-inline-block align-top" />
+            {' '}
+            Mooovies
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <Nav.Link href={`/films`}>Films</Nav.Link>
+            <Nav.Link href={`/users/${user.Username}`}>Profile</Nav.Link>
+            <Nav.Link href="#logout" onClick={logOut}>Logout</Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
 }
 
 export default NavbarView;
